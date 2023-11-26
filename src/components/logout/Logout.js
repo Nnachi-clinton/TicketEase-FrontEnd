3708d7e29fb470b6c218d5423aab8e0f0536bfb6
Nnachi-clinton <clinton.amadi@decagon.dev>
2023-11-26 15:19:25 +0100

    adding navigation hook

--- src/components/logout/Logout.js
+++ src/components/logout/Logout.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const LogoutPopout = ({ onLogout }) => {
   const [isVisible, setIsVisible] = useState(true);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleYesClick = () => {
+  const handleYesClick = async () => {
     setIsVisible(false);
     //navigate('/login'); // Navigate to the login page
   };
@@ -18,41 +18,43 @@ const LogoutPopout = ({ onLogout }) => {
 
   return (
     <>
-    <Moodal>
-      <Logout className={`popout ${isVisible ? 'visible' : 'hidden'}`}>
-        <Head>Logout Confirmation</Head>
-        <hr />
-        <p style={{ marginLeft: '30px' }}>Are you sure you want to logout from your account?</p>
-        <Btnwrap>
-          <Btn
-            onClick={handleNoClick}
-            isActive={false} // You can style the "No" button differently if needed
-          >
-            No
-          </Btn>
-          <Btn
-            onClick={handleYesClick}
-            isActive={true} // You can style the "Yes" button differently if needed
-          >
-            Yes
-          </Btn>
-        </Btnwrap>
-      </Logout>
+      <Moodal>
+        <Logout className={`popout ${isVisible ? 'visible' : 'hidden'}`}>
+          <Head>Logout Confirmation</Head>
+          <hr />
+          <p style={{ marginLeft: '30px' }}>
+            Are you sure you want to logout from your account?
+          </p>
+          <Btnwrap>
+            <Btn
+              onClick={handleNoClick}
+              isActive={false} // You can style the "No" button differently if needed
+            >
+              No
+            </Btn>
+            <Btn
+              onClick={handleYesClick}
+              isActive={true} // You can style the "Yes" button differently if needed
+            >
+              Yes
+            </Btn>
+          </Btnwrap>
+        </Logout>
       </Moodal>
     </>
   );
 };
 
-const Moodal= styled.div`
-width: 100vw;
-height: 100vh;
-top: 0;
-left: 0;
-background-color: rgba(0,0,0,0.724);
-align-items: center;
-jsutify-content: center;
-padding-top: 50px;
-position: absolute;
+const Moodal = styled.div`
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.724);
+  align-items: center;
+  jsutify-content: center;
+  padding-top: 50px;
+  position: absolute;
 `;
 
 const Logout = styled.div`
@@ -60,25 +62,25 @@ const Logout = styled.div`
   height: 200px;
   background: white;
   color: black;
-  margin:auto;
- position: relative;
+  margin: auto;
+  position: relative;
 `;
 
 const Btn = styled.button`
   background: ${({ isActive }) => (isActive ? '#505f98' : '#fcfcfc')};
   height: 30px;
   width: 60px;
-  padding:auto;
+  padding: auto;
   border: none;
   font-size: 16px;
-  border-radius:4px;
+  border-radius: 4px;
   color: ${({ isActive }) => (isActive ? '#fff' : '#000')};
 `;
 
 const Btnwrap = styled.div`
   //gap: 3px;
   margin-left: 356px;
-  margin-top:50px;
+  margin-top: 50px;
   display: flex;
   justify-content: space-evenly;
 `;
