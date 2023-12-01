630c1ac3a3c58475c93f920c706002746ac6fe28
Ifunanya <139649435+NanyaSyntax@users.noreply.github.com>
2023-12-01 16:38:43 +0100

    Implemented integration for ticket input field

--- src/pages/App.js
+++ src/pages/App.js
@@ -34,7 +34,7 @@ import CreateBoardEmptyManager from './CreateBoardEmptyManager';
 import TableData from './companyInfoTable/TableData';
 import CreateProject from './Projects/CreateProject';
 import CreateTicket from './TicketInputfield';
-import CreateNewTicket from './CreateNewTIcket';
+import CreateNewTicket from './CreateNewTicket';
 import TicketInputfield from '../pages/TicketInputfield';
 import PersonalInfo from './PersonalInfo.js';
 import EmailSent from '../components/checkEmail/EmailSent.js';
