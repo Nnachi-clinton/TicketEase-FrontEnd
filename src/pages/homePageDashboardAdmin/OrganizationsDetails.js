0b32c586915f6ceda2c7d5e37b22db5676a07fc5
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-05 14:30:53 +0100

    ViewOrganizationScreen

---
+++ src/pages/homePageDashboardAdmin/OrganizationsDetails.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import GeneralIcon from '../assets/ProfileDetails/General.svg';
+import PhoneIcon from '../assets/ProfileDetails/phone.svg';
+import CommunicationIcon from '../assets/ProfileDetails/communication.svg';
