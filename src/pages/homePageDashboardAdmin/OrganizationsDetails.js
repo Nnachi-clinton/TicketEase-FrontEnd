64e18353f176ac0b1fa19fb60c5d0ddf747eb0e2
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-05 14:33:44 +0100

    ViewOrganizationDetails

--- src/pages/homePageDashboardAdmin/OrganizationsDetails.js
+++ src/pages/homePageDashboardAdmin/OrganizationsDetails.js
@@ -4,3 +4,22 @@ import styled from 'styled-components';
 import GeneralIcon from '../assets/ProfileDetails/General.svg';
 import PhoneIcon from '../assets/ProfileDetails/phone.svg';
 import CommunicationIcon from '../assets/ProfileDetails/communication.svg';
+
+const Container = styled.div`
+  background-color: white;
+  width: 100%;
+  margin-top: 0.7em;
+  display: flex;
+  flex-direction: column; /* Adjusted to column layout */
+  align-items: center; /* Centered horizontally */
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  max-width: 1000px;
+`;
+
+const ProfileImage = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`;
